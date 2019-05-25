773bbda3dd6bb620d3c95a7c4410146aecae6cfb
Albert Szostkiewicz <tmdag@tmdag.com>
2019-05-25 15:16:32 -0700

    cleaning up and making it as a module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jsonparser',
+      version='0.1',
+      description='helper for parsing json files',
+      url='http://tmserver:3000/tmdag/jsonParser',
+      author='Albert Szostkiewicz',
+      author_email='tmdag@tmdag.com',
+      license='MIT',
+      packages=['jsonparser'],
+      zip_safe=False)
