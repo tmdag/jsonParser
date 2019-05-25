aeb3f2db2f2d2f38970d9837dd004b404707acea
Albert Szostkiewicz <tmdag@tmdag.com>
2019-05-25 15:36:29 -0700

    renamed source

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='jsonparser',
       version='0.3',
       description='helper for parsing json files',
-      url='http://tmserver:3000/tmdag/jsonParser',
+      url='https://github.com/tmdag/jsonParser',
       author='Albert Szostkiewicz',
       author_email='tmdag@tmdag.com',
       license='MIT',
